3b8dc95559793914a985750b270a58599894586f
artem_muntyanu <a.muntyanu@triplenext.com>
2025-07-21 18:26:47 +0100

    added course-card

--- angular-university-course/src/app/app.ts
+++ angular-university-course/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CourseCard } from "./course-card/course-card";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CourseCard],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('angular-university-course');
+  public readonly title = signal('angular-university-course');
+  
+  public handleChangeTitle(): void {
+    let newTitle = prompt('Change title', this.title());
+    if (newTitle === null || newTitle.trim() === '')
+      return;
+    this.title.set(newTitle);
+  }
 }
---
+++ angular-university-course/src/app/course-card/course-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'course-card',
+  imports: [],
+  templateUrl: './course-card.html',
+  styleUrl: './course-card.scss'
+})
+export class CourseCard {
+
+}
